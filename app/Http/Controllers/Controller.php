bd72e27926eb3ed9cc2553ba7f461ccf2d3bbff3
artem314 <artem_pochta_314@mail.ru>
2025-03-01 15:10:42 +0300

    main controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\dto\BaseApiInterface;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function JsonResponse(BaseApiInterface $dto): JsonResponse
+    {
+        return response()->json($dto->getData());
+    }
 }
