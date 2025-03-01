80fafa0d4657926b3695902079e00b5cea579f6b
artem314 <artem_pochta_314@mail.ru>
2025-03-01 15:11:37 +0300

    lang consts

---
+++ lang/en/base.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'success' => 'success',
+    'error' => 'error',
+
+    'created' => 'created',
+    'updated' => 'updated',
+    'deleted' => 'deleted',
+
+    'exception' => 'exception',
+];
